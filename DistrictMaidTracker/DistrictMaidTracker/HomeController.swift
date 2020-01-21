501cc9f98d463f386389790beb12bff030444a21
ej609 <ejohnson@routinesolution.com>
2020-01-20 17:20:31 -0800

    set up Django project

---
+++ DistrictMaidTracker/DistrictMaidTracker/HomeController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeController.swift
+//  DistrictMaidTracker
+//
+//  Created by Emmanuel Johnson on 11/4/18.
+//  Copyright © 2018 Emmanuel Johnson. All rights reserved.
+//
+
+import Foundation
